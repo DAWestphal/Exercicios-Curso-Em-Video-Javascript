331a3dee003fe883bb65d2f771b8720d4057d5d6
DAWestphal <56050283+DAWestphal@users.noreply.github.com>
2020-07-05 23:59:43 -0300

    Testing VS Code Commit

---
+++ aula12/ex012.js
@@ -0,0 +1,15 @@
+newFunction()
+
+function newFunction() {
+    var idade = 22
+    console.log(`Voce tem ${idade} anos`)
+    if (idade < 16) {
+        console.log('nao vota')
+    }
+    else if (idade < 18 || idade > 65) {
+        console.log('voto opcional')
+    }
+    else {
+        console.log('voto obrigatório')
+    }
+}
---
+++ aula12/ex013.js
@@ -0,0 +1,12 @@
+var hora = 25
+if (hora <= 5) {
+    console.log('bom madrugada')
+} else if (hora <= 12) {
+    console.log('bom dia')
+} else if (hora <= 18) {
+    console.log('boa tarde')
+} else if (hora <= 0) {
+    console.log("boa noite")
+} else {
+    console.log("essa nao é uma hora válida")
+}
