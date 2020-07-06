1b5bb77180333e8bc2776b03cd4491a680e22197
DAWestphal <56050283+DAWestphal@users.noreply.github.com>
2020-07-06 01:00:34 -0300

    Imagens alteradas

--- aula12ex/ex15/script.js
+++ aula12ex/ex15/script.js
@@ -8,17 +8,17 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         //bom dia
-        img.src = 'images/morning.png'
+        img.src = 'images/morning.gif'
         document.body.style.background = '#d39a95'
         msg.innerHTML += '<p>Bom dia!</p>'
     } else if (hora >= 12 && hora <= 18) {
         //boa tarde
-        img.src = 'images/noon.png'
+        img.src = 'images/noon.gif'
         document.body.style.background = '#88b3c0'
         msg.innerHTML += '<p>Boa tarde!</p>'
     } else {
         //boa noite
-        img.src = 'images/night.png'
+        img.src = 'images/night.gif'
         document.body.style.background = '#8f6162'
         msg.innerHTML += '<p>Boa noite!</p>'
     }
