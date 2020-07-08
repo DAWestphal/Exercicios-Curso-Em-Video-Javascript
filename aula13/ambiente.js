52f700f4ad067e6b0bf2301df1ea34cf5824cb11
DAWestphal <56050283+DAWestphal@users.noreply.github.com>
2020-07-08 12:00:23 -0300

    aula13 repetiçoes v2 teste lógico no final

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,7 +1,14 @@
 // estrutura de repetição com teste lógico no inicio
-var c = 0
-while (c <= 500) {
+var C = 0
+while (c < 6) {
    console.log(`Contador ${c}`)
    c++
 }
+
+// estrutura de repetição com teste lógico no final
+var c1 = 0
+do {
+    console.log(`Contador1 ${c1}`)
+    c1++
+} while (c1 < 6)
 
